35929d5ba9e418f9f63668b0831259c611888a65
AlesyaKorol <deadcandance666@gmail.com>
2017-04-08 00:16:55 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,27 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-      public static void main(String[] args) {  
-           System.out.println("Do or do not, there is no try.");
-       }
- }
+  public static void main(String[] args) {
+    yoda("Harry Potter");
+    yoda("Yoda");
+
+    double l = 9;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 5;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+  }
+
+  public static void yoda(String somebody) {
+    System.out.println("Do or do not, there is no try, said " + somebody);
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+}
