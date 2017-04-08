0210d41d4e23f651fe3353b78d25e6baa6361dbc
AlesyaKorol <deadcandance666@gmail.com>
2017-04-08 03:09:44 +0300

    Added distance counting logic

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -10,11 +10,19 @@ public class MyFirstProgram {
 
     Rectangle r = new Rectangle(5, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    Point p1 = new Point(5,5);
+    Point p2 = new Point(15,10);
+
+    System.out.println("Расстояние между точкой А с кординатами (" + p1.x
+            + "," + p1.y + ") и точкой B с кордbнатами" + "(" + p2.x + ","
+            + p2.y + ")" + " = " + p1.distance(p2));
   }
 
   public static void yoda(String somebody) {
     System.out.println("Do or do not, there is no try, said " + somebody);
   }
+
 
 
 }
