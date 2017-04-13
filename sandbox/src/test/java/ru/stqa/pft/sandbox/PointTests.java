85781f5e50f3e33ecee0a04f243595e50535d3eb
AlesyaKorol <deadcandance666@gmail.com>
2017-04-13 22:06:45 +0300

    Added tests for distance count between points.

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Alesia on 13.04.17.
+ */
+public class PointTests {
+
+  @Test
+  }
+
+  @Test
+  }
+
+  @Test
+  }
+
+}
