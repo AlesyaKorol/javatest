651ddeebb7c79d08820e402e080ef30166731d1b
AlesyaKorol <deadcandance666@gmail.com>
2017-04-15 00:17:58 +0300

    Added test to create new contact.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String lastname;
+  private final String mobilephone;
+  private final String email1;
+
+  public ContactData(String firstname, String lastname, String mobilephone, String email1) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.mobilephone = mobilephone;
+    this.email1 = email1;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getMobilephone() {
+    return mobilephone;
+  }
+
+  public String getEmail1() {
+    return email1;
+  }
+}
