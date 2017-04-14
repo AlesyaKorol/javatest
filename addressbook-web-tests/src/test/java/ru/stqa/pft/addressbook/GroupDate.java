54d882dbc37c69cf56216a6ca41a4b7e51cbff6d
AlesyaKorol <deadcandance666@gmail.com>
2017-04-14 23:43:53 +0300

    Added class GroupData

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDate.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.addressbook;
+
+public class GroupDate {
+  private final String groupname;
+  private final String header;
+  private final String footer;
+
+  public GroupDate(String groupname, String header, String footer) {
+    this.groupname = groupname;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public String getGroupname() {
+    return groupname;
+  }
+
+  public String getHeader() {
+    return header;
+  }
+
+  public String getFooter() {
+    return footer;
+  }
+}
