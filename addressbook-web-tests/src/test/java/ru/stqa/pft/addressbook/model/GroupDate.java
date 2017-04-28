3bb97e766331d80fa77b4727a8044d486cb620dd
AlesyaKorol <deadcandance666@gmail.com>
2017-04-28 23:53:16 +0300

    Collection comparison for GroupsCreationTests.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupDate.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupDate.java
@@ -1,12 +1,12 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupDate {
-  private final String id;
+  private int id;
   private final String groupname;
   private final String header;
   private final String footer;
 
-  public GroupDate(String id, String groupname, String header, String footer) {
+  public GroupDate(int id, String groupname, String header, String footer) {
     this.id = id;
     this.groupname = groupname;
     this.header = header;
@@ -14,14 +14,14 @@ public class GroupDate {
   }
 
 
-  public GroupDate( String groupname, String header, String footer) {
+  public GroupDate(String groupname, String header, String footer) {
     this.groupname = groupname;
     this.header = header;
     this.footer = footer;
-    this.id = null;
+    this.id = 0;
   }
 
-  public String getId() {
+  public int getId() {
     return id;
   }
 
@@ -37,23 +37,8 @@ public class GroupDate {
     return footer;
   }
 
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupDate groupDate = (GroupDate) o;
-
-    if (id != null ? !id.equals(groupDate.id) : groupDate.id != null) return false;
-    return groupname != null ? groupname.equals(groupDate.groupname) : groupDate.groupname == null;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id != null ? id.hashCode() : 0;
-    result = 31 * result + (groupname != null ? groupname.hashCode() : 0);
-    return result;
+  public void setId(int id) {
+    this.id = id;
   }
 
   @Override
@@ -64,6 +49,22 @@ public class GroupDate {
             '}';
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
 
+    GroupDate groupDate = (GroupDate) o;
+
+    if (id != groupDate.id) return false;
+    return groupname != null ? groupname.equals(groupDate.groupname) : groupDate.groupname == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (groupname != null ? groupname.hashCode() : 0);
+    return result;
+  }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupsCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupsCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupsCreationTests extends TestBase {
@@ -12,9 +13,21 @@ public class GroupsCreationTests extends TestBase {
   public void testGroupsCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupDate> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createAGroup(new GroupDate("group1","Group1-h", "Group1-f"));
+    GroupDate group = new GroupDate("group1", "Group1-h", "Group1-f");
+    app.getGroupHelper().createAGroup(group);
     List<GroupDate> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(), before.size() +1 );
+    Assert.assertEquals(after.size(), before.size() + 1);
+
+
+    int max = 0;
+    for (GroupDate g : after) {
+      if (g.getId() > max) {
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    before.add(group);
+    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
 
 }
