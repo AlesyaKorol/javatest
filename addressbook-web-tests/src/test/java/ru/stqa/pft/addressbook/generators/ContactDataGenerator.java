3668d42af7e87186b852883466ab538a7eb29c66
AlesyaKorol <deadcandance666@gmail.com>
2017-05-11 20:10:38 +0300

    Added ContactDataGenerator.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,47 @@
+package ru.stqa.pft.addressbook.generators;
+
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Alesia on 11.05.17.
+ */
+public class ContactDataGenerator {
+
+  public static void main (String [] args) throws IOException {
+    int count = Integer.parseInt(args[0]);
+    File file = new File(args[1]);
+
+    List<ContactData> contacts = generateContacts(count);
+    save (contacts, file);
+
+  }
+
+  private static void save(List<ContactData> contacts, File file) throws IOException {
+    Writer writer = new FileWriter(file);
+    for (ContactData contact: contacts){
+      writer.write(String.format("%s; %s; %s; %s; %s; %s\n", contact.getFirstname(), contact.getLastname(),
+      contact.getHomephone(), contact.getMobilephone(), contact.getWorkphone(), contact.getEmail()));
+    }
+    writer.close();
+  }
+
+  private static List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for (int i = 0; i < count; i++){
+      contacts.add(new ContactData().withFirstname(String.format("firstname %s", i))
+      .withLastname(String.format("lastname %s", i)).withHomephone(String.format("111 %s", i))
+      .withMobilephone(String.format("222 %s", i)).withWorkphone(String.format("333 %s", i))
+      .withEmai1(String.format("email%s@test.com", i)));
+    }
+    return contacts;
+  }
+
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.addressbook.generators;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,7 +14,7 @@ import java.util.List;
  */
 public class GroupDataGenerator {
 
-  public static void main (String args[]){
+  public static void main (String [] args) throws IOException {
 
    int count = Integer.parseInt(args[0]);
    File file = new File(args[1]);
@@ -20,7 +23,13 @@ public class GroupDataGenerator {
     save (groups, file);
   }
 
-  private static void save(List<GroupDate> groups, File file) {
+  private static void save(List<GroupDate> groups, File file) throws IOException {
+    System.out.println(new File(".").getAbsolutePath());
+    Writer writer = new FileWriter(file);
+    for (GroupDate group: groups){
+      writer.write(String.format("%s; %s; %s\n", group.getGroupname(), group.getHeader(), group.getFooter()));
+    }
+    writer.close();
   }
 
   private static List<GroupDate> generateGroups(int count) {
