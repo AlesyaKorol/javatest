abade8e466baccd8f6112429870a5a456b57c3fb
AlesyaKorol <deadcandance666@gmail.com>
2017-05-12 00:37:31 +0300

    Load test data from file for creation tests.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -5,7 +5,10 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,14 +19,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTest extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts(){
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new ContactData().withFirstname("firstname1").withLastname("lastname1")
-            .withAddress("address1").withHomephone("111").withMobilephone("222").withWorkphone("333")
-            .withEmai1("email1").withEmail2("email2").withEmail3("email3").withGroup("[none]")});
-    list.add(new Object[] {new ContactData().withFirstname("firstname2").withLastname("lastname2")
-            .withAddress("address2").withHomephone("111-2").withMobilephone("222-2").withWorkphone("333-2")
-            .withEmai1("email1-2").withEmail2("email2-2").withEmail3("email3-2").withGroup("[none]")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1])
+      .withAddress(split[2]).withHomephone(split[3]).withMobilephone(split[4]).withWorkphone(split[5])
+      .withEmai1(split[6]).withEmail2(split[7]).withEmail3(split[8]).withGroup(split[9])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupsCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupsCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDate;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,12 +16,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupsCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() {
+  public Iterator<Object[]> validGroups() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[]{new GroupDate().withName("test 1").withHeader("header 1").withFooter("footer 1")});
-    list.add(new Object[]{new GroupDate().withName("test 2").withHeader("header 2").withFooter("footer 2")});
-    list.add(new Object[]{new GroupDate().withName("test 3").withHeader("header 3").withFooter("footer 3")});
-    list.add(new Object[]{new GroupDate().withName("test 4")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null){
+      String[] split = line.split(";");
+      list.add(new Object[] {new GroupDate().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
