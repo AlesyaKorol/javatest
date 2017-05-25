695a7e6a043d32e688983566cd3c6bf7de354ae1
AlesyaKorol <deadcandance666@gmail.com>
2017-05-25 12:43:15 +0300

    Added GroupRemoveContactTests .

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupRemoveContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupRemoveContactTests.java
@@ -6,16 +6,17 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by Alesia on 23.05.17.
  */
-public class ContactDeleteFromGroupTests extends TestBase {
+public class GroupRemoveContactTests extends TestBase {
   @BeforeMethod
   public void ensurePreconditions() {
-     Groups groups = app.db().groups();
-      if (app.db().groups().size() == 0) {
+    Groups groups = app.db().groups();
+    if (app.db().groups().size() == 0) {
       app.goTo().groupPage();
       app.group().create(new GroupData().withName("test1"));
     }
@@ -26,26 +27,30 @@ public class ContactDeleteFromGroupTests extends TestBase {
               withFirstname("Olga02").withLastname("Test").withAddress("test")
               .inGroup(groups.iterator().next()), true);
     }
+
     app.goTo().homePage();
   }
 
 
   @Test
-  public void testContactDeleteFromGroup (){
+  public void testGroupRemoveContact() {
     Groups before = app.db().groups();
-  GroupData groupInTopList = before.iterator().next();
+    GroupData groupInTopList = before.iterator().next();
+    GroupData group = new GroupData().withName(groupInTopList.getName());
 
-    GroupData group = new GroupData().withName("test1");
+    if (group.getContacts().size() == 0) {
+      app.goTo().homePage();
+      app.contact().create(new ContactData().
+              withFirstname("Olga02").withLastname("Test").withAddress("test")
+              .inGroup(group), true);
+    }
 
     app.group().deleteFromGroup(group);
-    app.goTo().groupPage();
-   // app.group().removeTopGroupSelection();
-
     Groups after = app.db().groups();
 
-    //assertThat(after, equalTo(before.without(contact.withGroups(groups)).withAdded(contactToDelete)));
+    assertThat(after, equalTo(before));
   }
 
-  }
+}
 
 
