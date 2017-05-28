02333855e5498ea8fee2b59c44544f22ed9b5abb
AlesyaKorol <deadcandance666@gmail.com>
2017-05-28 23:20:04 +0300

    Added method finish to RegistrationHelper.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -3,6 +3,12 @@ package ru.stqa.pft.mantis.tests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Alesia on 27.05.17.
@@ -14,9 +20,20 @@ public class RegistrationTests extends TestBase {
   }
 
   @Test
-  public void testRegistration(){
-    app.registration().start ("user1", "user1@localhost.localhostdomain");
+  public void testRegistration() throws IOException, MessagingException {
+    String email = "user1@localhost.localhostdomain";
+    app.registration().start ("user1", email);
+    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    String confirmationLink = findConfirmationLink(mailMessages, email);
+    app.registration().finish (confirmationLink, "password");
 
+  }
+
+  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+    MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+    VerbalExpression regex = VerbalExpression.regex().find("http://")
+            .nonSpace().oneOrMore().build();
+   return regex.getText(mailMessage.text);
   }
 
   @AfterMethod (alwaysRun = true)
