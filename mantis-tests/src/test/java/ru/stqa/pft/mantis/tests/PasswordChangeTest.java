2308a0eb516e2407fe0c1f358d623788e26a4353
AlesyaKorol <deadcandance666@gmail.com>
2017-05-30 00:15:40 +0300

    Changes for testPasswordChange

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/PasswordChangeTest.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/PasswordChangeTest.java
@@ -1,10 +1,14 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.AssertJUnit;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.mantis.appmanager.HttpSession;
 import ru.stqa.pft.mantis.model.MailMessage;
+import ru.stqa.pft.mantis.model.UserData;
+import ru.stqa.pft.mantis.model.Users;
+import ru.lanwen.verbalregex.VerbalExpression;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
@@ -24,15 +28,18 @@ public class PasswordChangeTest extends TestBase {
 
   @Test
   public void testPasswordChange() throws IOException, MessagingException {
+    String password = "pass";
+    Users before = app.db().users();
+    UserData userToChange = before.iterator().next();
+    UserData user = new UserData().withId(userToChange.getId()).withUsername(userToChange.getUsername()).withEmail(userToChange.getEmail());
     HttpSession session = app.newSession();
     assertTrue(session.login("administrator", "root"));
-    app.manage().changePasswordStart();
-    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
-    String confirmationLink = app.mail().findConfirmationLink(mailMessages, email);
-
-
-
-  }
+    app.manage().changePasswordStart(user);
+    List<MailMessage> mailMessages = app.mail().waitForMail(1, 10000);
+    String confirmationLink = app.mail().findConfirmationLink(mailMessages, user.getEmail());
+    app.manage().changePasswordFinish(confirmationLink, password);
+    assertTrue(app.newSession().login(user.getUsername(),password));
+    }
 
   @AfterMethod(alwaysRun = true)
   public void stopMailServer() {
