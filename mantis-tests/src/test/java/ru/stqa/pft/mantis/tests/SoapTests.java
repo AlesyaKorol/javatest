15b7c0714307877846d79688fc818b360f32cf76
AlesyaKorol <deadcandance666@gmail.com>
2017-06-01 23:39:41 +0300

    Added SoapTests for Mantis.

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.mantis.tests;
+
+import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
+import biz.futureware.mantis.rpc.soap.client.ProjectData;
+import org.testng.annotations.Test;
+
+import javax.xml.rpc.ServiceException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
+
+/**
+ * Created by Alesia on 01.06.17.
+ */
+public class SoapTests {
+
+  @Test
+  public void testGetProjects() throws MalformedURLException, ServiceException, RemoteException {
+    MantisConnectPortType mc = new MantisConnectLocator()
+            .getMantisConnectPort(new URL("http://localhost/mantisbt-1.2.19/api/soap/mantisconnect.php"));
+    ProjectData[] projects = mc.mc_projects_get_user_accessible("administrator", "root");
+  }
+}
