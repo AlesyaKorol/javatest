0a79898ceec2521c65a5e384949cc6dd88281a58
AlesyaKorol <deadcandance666@gmail.com>
2017-06-08 15:52:15 +0300

    Added intagration with BugifyBT.

--- rest-sample/src/test/java/ru/stqa/pft/rest/TestBase.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/TestBase.java
@@ -1,25 +1,13 @@
 package ru.stqa.pft.rest;
 
-import biz.futureware.mantis.rpc.soap.client.IssueData;
-import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
-import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
-import org.openqa.selenium.remote.BrowserType;
 import org.testng.SkipException;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
-import javax.xml.rpc.ServiceException;
-import java.io.File;
 import java.io.IOException;
-import java.math.BigInteger;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.rmi.RemoteException;
 
 
 /**
@@ -30,23 +18,26 @@ public class TestBase {
 
   public boolean isIssueOpen(int issueId) throws IOException {
 
-    String json = getExecutor().execute(Request.Get(String.format("http://demo.bugify.com/api/issues/%s.json", issueId)))
+    String json = getExecutor()
+            .execute(Request.Get(String.format("http://demo.bugify.com/api/issues/%s.json", issueId)))
             .returnContent().asString();
 
     JsonElement parsed = new JsonParser().parse(json);
-    JsonElement issue = parsed.getAsJsonObject().get("issues");
+    JsonArray issues = parsed.getAsJsonObject().get("issues").getAsJsonArray();
+    JsonElement element = issues.get(0);
+    String status = element.getAsJsonObject().get("state_name").getAsString();
 
-    if (issue.get().getName().equals("open")) {
+    if(  status.equals("Open")){
       return true;
     }
-    if (!issue.getResolution().getName().equals("open")) {
+    if( ! status.equals("Open")){
       return false;
     }
     return false;
   }
 
 
-  private Executor getExecutor() {
+ public Executor getExecutor() {
     return Executor.newInstance().auth("LSGjeU4yP1X493ud1hNniA==", "");
   }
 
